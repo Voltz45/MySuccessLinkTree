3544a43e1b3d4f9f3971e49858d54954d2ab5ca9
Chikko45 <chikko45putra@gmail.com>
2023-02-03 16:28:30 +0700

    INITIAL COMMIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {MatDialog} from "@angular/material/dialog";
+import {CsComponent} from "./component/cs/cs.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MySuccessLinkTree';
+  urls: any = {
+    list_url: [
+      {
+        url: ""
+      },
+      {
+        url: ""
+      },
+      {
+        url: ""
+      },
+      {
+        url: ""
+      },
+      {
+        url: ""
+      },
+      {
+        url: ""
+      },
+      {
+        url: ""
+      }
+    ]
+  }
+
+  constructor(
+    private db: AngularFirestore,
+    private dialog: MatDialog
+  ) {
+    this.db.collection('hyperlink').snapshotChanges()
+      .subscribe(response => {
+        this.urls = response[0].payload.doc.data();
+      })
+  }
+
+  ngOnInit() {
+    this.headerEffect()
+  }
+
+  headerEffect() {
+    window.addEventListener('scroll', function () {
+      navbarScroll();
+    });
+
+    function navbarScroll() {
+      const y = window.scrollY;
+      const header = document.getElementsByClassName('first-container')[0];
+      const logo = document.getElementsByClassName('logo-container')[0];
+
+
+      if (y > 10) {
+        header.classList.add('small');
+        logo.classList.add('hide');
+      } else if (y < 10) {
+        header.classList.remove('small');
+        logo.classList.remove('hide');
+      }
+    }
+  }
+
+  click() {
+    this.dialog.open(CsComponent)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { CsComponent } from './component/cs/cs.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatButtonModule} from "@angular/material/button";
+
+const config = {
+  apiKey: "AIzaSyDG1f3ZAMKaWKgFlry7YsRE0m0SyCDYraA",
+  authDomain: "my-success-2022.firebaseapp.com",
+  projectId: "my-success-2022",
+  storageBucket: "my-success-2022.appspot.com",
+  messagingSenderId: "49289699269",
+  appId: "1:49289699269:web:23694d5059813831d335ac"
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatButtonModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
   ],
   providers: [],
   bootstrap: [AppComponent]
